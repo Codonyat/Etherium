12cbc44e7a66bbe62b7153c7f30943fdde468f55
Codonyat <nil.garcia@gmail.com>
2025-11-14 17:13:44 +0100

    improved deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,18 +4,31 @@ pragma solidity ^0.8.20;
 import {Script, console} from "forge-std/Script.sol";
 import {Strategy} from "../src/Strategy.sol";
 
+/** @dev deployment:
+    With Ledger:
+    forge script script/Deploy.s.sol --rpc-url <RPC_URL> --broadcast --ledger --hd-paths $HD_PATH
+
+    With Private Key:
+    forge script script/Deploy.s.sol --rpc-url <RPC_URL> --broadcast --private-key $PRIVATE_KEY
+*/
 contract DeployScript is Script {
-    function setUp() public {}
+    address wmonAddress;
+
+    function setUp() public {
+        // Monad Mainnet
+        if (block.chainid == 143) {
+            wmonAddress = 0x3bd359C1119dA7Da1D913D1C4D2B7c461115433A;
+        }
+        // Monad Testnet
+        else if (block.chainid == 10143) {
+            wmonAddress = 0x760AfE86e5de5fa0Ee542fc7B7B713e1c5425701;
+        } else {
+            revert("Not a supported chain.");
+        }
+    }
 
     function run() public {
-        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-
-        // Get WMON address from env (required for auctions)
-        // Monad Mainnet WMON: 0x3bd359C1119dA7Da1D913D1C4D2B7c461115433A
-        // Monad Testnet WMON: 0x760AfE86e5de5fa0Ee542fc7B7B713e1c5425701
-        address wmonAddress = vm.envAddress("WMON_ADDRESS");
-
-        vm.startBroadcast(deployerPrivateKey);
+        vm.startBroadcast();
 
         Strategy monstr = new Strategy(wmonAddress);
 
